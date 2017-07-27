44ad5a49b91accac24337ce197e220a534e8f3fa
Parth Shah <parthks@umich.edu>
2017-07-26 22:13:28 -0400

    Phew! ASCII Art done basic
    hacked it together lol. Took ages for the width character alignment.
    Auto font making to fill the width of the screen was pretty cool
    graphing with desmos.
    Functional enough to sorta use rn. so done? for now! phew..

---
+++ asciiart/asciiart-play.js
@@ -0,0 +1,191 @@
+var Width = 300;
+var Height = 300;
+var picSource;
+
+$(document).ready(function() {
+    console.log('ASCII ART');
+    var pic = new Image();
+    pic.src = 'randomimage.png';
+    picSource = pic.src;
+    make_canvas(pic.src);
+
+    $('#img1').click(function(e){
+        var pic = new Image();
+        pic.src = 'randomimage.png';
+        picSource = pic.src;
+        make_canvas(pic.src);
+    });
+
+    $('#img2').click(function(e){
+        var pic = new Image();
+        pic.src = 'starrynight.jpg';
+        picSource = pic.src;
+        make_canvas(pic.src);
+    }); 
+
+    $('#img3').click(function(e){
+        var pic = new Image();
+        pic.src = 'monalisa.jpg';
+        picSource = pic.src;
+        make_canvas(pic.src);
+    }); 
+
+    $("input[name='min-height']").change(function(e){
+        console.log("hiii");
+        Height = parseInt($("input[name='min-height']").val());
+        make_canvas(picSource);
+    });
+
+    $("input[name='min-width']").change(function(e){
+        Width = parseInt($("input[name='min-width']").val());
+        make_canvas(picSource);
+    });   
+
+$('#submit').click(function(e){
+    
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var rw = c.width;
+    var rh = c.height;
+
+    var s4 = $("select[name=dark]").val();
+    var s3 = $("select[name=medium]").val();
+    var s2 = $("select[name=light]").val();
+    var s1 = $("select[name=lightest]").val();
+    
+    var hcount =  parseInt($("input[name=height]").val());
+    var wcount = parseInt($("input[name=width]").val());
+    console.log(hcount);
+    console.log('w',wcount);
+    console.log('rw',rw,'rh',rh);
+    var imgData = ctx.getImageData(0,0,rw,rh);
+    //console.log(imgData);
+    var count = 0;
+    var imgArray = '';
+
+    var row = rw/wcount;
+
+    for (var i=0;i<imgData.data.length;i+=(4*wcount)){
+        count += wcount;
+        var sum = 0;
+        //console.log('count: ',count);
+        //console.log('i: ',i);
+        for (var j = 0; j < hcount; j++) {
+            for (var k = 0; k < wcount; k++) {
+                var avg1 = (imgData.data[i+(j*rw*4)+(k*4)]+imgData.data[i+1+(j*rw*4)+(k*4)]+imgData.data[i+2+(j*rw*4)+(k*4)])/3;
+                //console.log('index: ',i+(j*rw*4)+(k*4));
+                sum += avg1;
+            }
+            
+            
+        }
+        // var avg1 = (imgData.data[i]+imgData.data[i+1]+imgData.data[i+2])/3;
+        // var avg2 = (imgData.data[i+(rw*4)]+imgData.data[i+1+(rw*4)]+imgData.data[i+2+(rw*4)])/3;
+        var avg = sum / (hcount+wcount);
+        if((hcount == 1) || (wcount == 1)){
+            avg = sum / (hcount+wcount-1);
+        }
+        
+        if (avg < 63.75){
+            imgArray += s1;
+        } else if (avg < 63.75*2) {
+            imgArray += s2
+        } else if (avg < 63.75*3) {
+            imgArray += s3;
+        } else {
+            imgArray += s4;
+        }
+        //console.log(num);
+        //console.log(imgArray);
+        if (count >= rw){
+            //console.log('count: ',count);
+            count = 0;
+            imgArray += '\n';
+            i += ((hcount-1)*rw*4);
+            //console.log('i: ',i);
+        } 
+
+        
+        //imgData.data[i + 3] = 255;   
+    }
+    
+    //console.log(imgArray);
+    var wnd = window.open("about:blank", "", "_blank");
+
+    wnd.document.write("<span id='hi'>"+imgArray+'</span>');
+    var y = 2850.0 / row;
+    console.log(y);
+    wnd.document.getElementById("hi").style.fontSize = y;
+
+
+    
+    // $(wnd.document).ready(function(){
+    //     wnd.document.getElementById("hi").style.fontSize = "xx-small";
+    // });
+    
+
+});
+
+});
+
+
+function make_canvas(newImageSource){
+
+    var img = new Image();
+    img.onload = function() {
+        var w = parseInt(img.width || img.naturalWidth);
+        var h = parseInt(img.height || img.naturalHeight);
+
+        var ratio = 1;
+        console.log('w=',Width,'h=',Height);
+        if (Width < Height){
+            console.log('width=',Width,'h=',Height);
+            ratio = Width / img.width;
+        } else {
+            console.log('w=',Width,'height=',Height);
+            ratio = Height / img.height;
+        }
+            
+
+        var canvas = document.createElement('canvas');
+        canvas.id = "myCanvas";
+        canvas.width = w * ratio;
+        canvas.height = h * ratio;
+
+        // $('#img-width').empty();
+        // $('#img-width').empty();
+        $('#img-width').html("Width = "+canvas.width);
+        $('#img-height').html("Height = "+canvas.height);
+
+        $("#images").empty();
+        $("#images").append('<br>');
+        $("#images").append(canvas);
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img,0,0,canvas.width,canvas.height);
+        
+    };
+    img.src = newImageSource;
+
+    
+    //var img = document.getElementById("source");
+    
+}
+
+
+
+function previewFile() {
+    var file = document.querySelector('input[type=file]').files[0]; //sames as here
+    var reader = new FileReader();
+
+    reader.onloadend = function() {
+        console.log(reader.result);
+        picSource = reader.result;
+        make_canvas(reader.result);
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    } else {
+        alert("Error reading file :(");
+    }
+}
--- asciiart/asciiart.js
+++ asciiart/asciiart.js
@@ -1,26 +1,32 @@
 $(document).ready(function() {
     console.log('ASCII ART');
+    
+
+$('#asciify').click(function(e){
     var img = document.getElementById("source");
-    var w = img.width || image.naturalWidth;
-    var h = img.height || image.naturalHeight;
+    var w = img.width || img.naturalWidth;
+    var h = img.height || img.naturalHeight;
+
+    var rw = w;
+    var rh = h;
 
     var canvas = document.createElement('canvas');
     canvas.id = "myCanvas";
-    canvas.width = w;
-    canvas.height = h;
+    canvas.width = rw;
+    canvas.height = rh;
     $("#images").append(canvas);
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    
-    ctx.drawImage(img,0,0);
-    var imgData = ctx.getImageData(0,0,w,h);
+    console.log(rw);
+    ctx.drawImage(img,0,0,rw,rh);
+    var imgData = ctx.getImageData(0,0,rw,rh);
     console.log(imgData);
     var count = 0
     var imgArray = '';
     for (var i=0;i<imgData.data.length;i+=4){
         count += 1
         var avg1 = (imgData.data[i]+imgData.data[i+1]+imgData.data[i+2])/3;
-        var avg2 = (imgData.data[i+(362*4)]+imgData.data[i+1+(362*4)]+imgData.data[i+2+(362*4)])/3;
+        var avg2 = (imgData.data[i+(rw*4)]+imgData.data[i+1+(rw*4)]+imgData.data[i+2+(rw*4)])/3;
         var avg = (avg1 + avg2) / 2
         if (avg < 63.75){
             imgArray += '1';
@@ -31,18 +37,16 @@ $(document).ready(function() {
         } else {
             imgArray += '8';
         }
-        if (count == 362){
+        if (count == rw){
             count = 0;
             imgArray += '\n';
-            i += (362*4);
+            i += (rw*4);
         }   
         imgData.data[i + 3] = 255;   
     }
-//console.log(imgArray);
-$('#myCanvas').remove();
-//ctx.putImageData(imgData,0,0);
-
-$('#asciify').click(function(e){
+    //console.log(imgArray);
+    $('#myCanvas').remove();
+    //ctx.putImageData(imgData,0,0);
     var wnd = window.open("about:blank", "", "_blank");
     wnd.document.write(imgArray);
     wnd.parent.document.body.style.zoom = 0.1;
