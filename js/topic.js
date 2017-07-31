f727c32206e2959207be66eea2fce6ba3bbf64c6
Parth Shah <parthks@umich.edu>
2017-07-30 22:40:36 -0400

    Fixed Custom Calculator bug

--- js/topic.js
+++ js/topic.js
@@ -320,7 +320,7 @@ function customGoBackButton() {
         '<div id="CustomAppButton" class="col-md-1 center">' +
             '<button class="btn btn-secondary" onclick="clickCustomButton(this);">Application</button>' +
         '</div>' +
-        '<div id="CustomExtraSpace" class="col-md-3 center"></div>';
+        '<div id="CustomExtraSpace" class="col-md-4 center"></div>';
 
     $('#CustomBoxesTitle').after(html);
   });
@@ -347,7 +347,7 @@ function clickCustomButton(ele){
   var html = '<div id="customCurrentTitle" class="col-md-2 center">' +
             '<h3 style="padding-top: 3px">'+$(ele).text()+': </h3>'+
          '</div>'+
-        '<div id="customOptions" class="col-md-4 center">';
+        '<div id="customOptions" class="col-md-5 center">';
 
   for (var i = 0; i < names.length; i++) {
     html += '<button onclick="CustomBoxButton(this);" class="btn btn-secondary">'+names[i]+'</button>'+
@@ -410,6 +410,12 @@ function addCustomBox(id, method){
       if (id=='#appDivBigBoy') {
         $(id).css('width','500px');
         $(id).css('height','500px');
+      }
+
+      if (id == '#calculatorDivBigBoy') {
+        $(id).css('width','329px');
+        $(id).css('height','471px');
+        calculator();
       }
 
   }
@@ -855,17 +861,15 @@ function getNoteData(){
     });
 }
 
-function getReviewTextBox() {
-    return '<div id = "revTextBigBoy" onclick = "sendontop(this);" class = "ui-widget-content big-boy" >' +
-        '<div id = "revtext" class = "box" >' +
-        '<div class = "ui-widget-header">' +
-        '<h3> Review Concepts </h3>' +
-        '<button onclick="CustomCloseBox(this)" class="close-button-right">X</button>' +
-        '</div>' +
-        reviewText() +
-        '</div>' +
-        '</div>';
-        
+function getReviewTextBox() {return '<div id = "revTextBigBoy" onclick = "sendontop(this);" class = "ui-widget-content big-boy" >' +
+    '<div id = "revtext" class = "box" >' +
+    '<div class = "ui-widget-header">' +
+    '<h3> Review Concepts </h3>' +
+    '<button onclick="CustomCloseBox(this)" class="close-button-right">X</button>' +
+    '</div>' +
+    reviewText() +
+    '</div>' +
+    '</div>';        
     
 }
 
@@ -873,9 +877,9 @@ function getReviewTextBox() {
 function getSummaryTextBox() {
     return '<div id = "summTextBigBoy" onclick = "sendontop(this);" class = "ui-widget-content big-boy" >' +
         '<div id = "summText" class = "box" >' +
-        '<div class = "ui-widget-header">'+
+        '<div class = "ui-widget-header">' +
         '<h3 > Summary Text </h3>' +
-        '<button onclick="CustomCloseBox(this)" class="close-button-right">X</button>'+
+        '<button onclick="CustomCloseBox(this)" class="close-button-right">X</button>' +
         '</div>' +
         summaryText() +
         '</div' +
@@ -909,122 +913,122 @@ function getSummaryTextBox() {
 
 function calculator() {
     var input = document.getElementById('input'), // input/output button
-      number = document.querySelectorAll('.numbers div'), // number buttons
-      operator = document.querySelectorAll('.operators div'), // operator buttons
-      result = document.getElementById('result'), // equal button
-      clear = document.getElementById('clear'), // clear button
-      resultDisplayed = false; // flag to keep an eye on what output is displayed
+        number = document.querySelectorAll('.numbers div'), // number buttons
+        operator = document.querySelectorAll('.operators div'), // operator buttons
+        result = document.getElementById('result'), // equal button
+        clear = document.getElementById('clear'), // clear button
+        resultDisplayed = false; // flag to keep an eye on what output is displayed
 
     // adding click handlers to number buttons
     for (var i = 0; i < number.length; i++) {
-      number[i].addEventListener("click", function(e) {
+        number[i].addEventListener("click", function(e) {
+            console.log('CLICKZED NUM!');
+            // storing current input string and its last character in variables - used later
+            var currentString = input.innerHTML;
+            var lastChar = currentString[currentString.length - 1];
 
-        // storing current input string and its last character in variables - used later
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
+            // if result is not diplayed, just keep adding
+            if (resultDisplayed === false) {
+                input.innerHTML += e.target.innerHTML;
+            } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+                // if result is currently displayed and user pressed an operator
+                // we need to keep on adding to the string for next operation
+                resultDisplayed = false;
+                input.innerHTML += e.target.innerHTML;
+            } else {
+                // if result is currently displayed and user pressed a number
+                // we need clear the input string and add the new input to start the new opration
+                resultDisplayed = false;
+                input.innerHTML = "";
+                input.innerHTML += e.target.innerHTML;
+            }
 
-        // if result is not diplayed, just keep adding
-        if (resultDisplayed === false) {
-          input.innerHTML += e.target.innerHTML;
-        } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-          // if result is currently displayed and user pressed an operator
-          // we need to keep on adding to the string for next operation
-          resultDisplayed = false;
-          input.innerHTML += e.target.innerHTML;
-        } else {
-          // if result is currently displayed and user pressed a number
-          // we need clear the input string and add the new input to start the new opration
-          resultDisplayed = false;
-          input.innerHTML = "";
-          input.innerHTML += e.target.innerHTML;
-        }
-
-      });
+        });
     }
 
     // adding click handlers to number buttons
     for (var i = 0; i < operator.length; i++) {
-      operator[i].addEventListener("click", function(e) {
+        operator[i].addEventListener("click", function(e) {
 
-        // storing current input string and its last character in variables - used later
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
+            // storing current input string and its last character in variables - used later
+            var currentString = input.innerHTML;
+            var lastChar = currentString[currentString.length - 1];
 
-        // if last character entered is an operator, replace it with the currently pressed one
-        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-          var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
-          input.innerHTML = newString;
-        } else if (currentString.length == 0) {
-          // if first key pressed is an opearator, don't do anything
-          console.log("enter a number first");
-        } else {
-          // else just add the operator pressed to the input
-          input.innerHTML += e.target.innerHTML;
-        }
+            // if last character entered is an operator, replace it with the currently pressed one
+            if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+                var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+                input.innerHTML = newString;
+            } else if (currentString.length == 0) {
+                // if first key pressed is an opearator, don't do anything
+                console.log("enter a number first");
+            } else {
+                // else just add the operator pressed to the input
+                input.innerHTML += e.target.innerHTML;
+            }
 
-      });
+        });
     }
 
     // on click of 'equal' button
     result.addEventListener("click", function() {
 
-      // this is the string that we will be processing eg. -10+26+33-56*34/23
-      var inputString = input.innerHTML;
+        // this is the string that we will be processing eg. -10+26+33-56*34/23
+        var inputString = input.innerHTML;
 
-      // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-      var numbers = inputString.split(/\+|\-|\×|\÷/g);
+        // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
+        var numbers = inputString.split(/\+|\-|\×|\÷/g);
 
-      // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-      // first we replace all the numbers and dot with empty string and then split
-      var operators = inputString.replace(/[0-9]|\./g, "").split("");
+        // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+        // first we replace all the numbers and dot with empty string and then split
+        var operators = inputString.replace(/[0-9]|\./g, "").split("");
 
-      console.log(inputString);
-      console.log(operators);
-      console.log(numbers);
-      console.log("----------------------------");
+        console.log(inputString);
+        console.log(operators);
+        console.log(numbers);
+        console.log("----------------------------");
 
-      // now we are looping through the array and doing one operation at a time.
-      // first divide, then multiply, then subtraction and then addition
-      // as we move we are alterning the original numbers and operators array
-      // the final element remaining in the array will be the output
+        // now we are looping through the array and doing one operation at a time.
+        // first divide, then multiply, then subtraction and then addition
+        // as we move we are alterning the original numbers and operators array
+        // the final element remaining in the array will be the output
 
-      var divide = operators.indexOf("÷");
-      while (divide != -1) {
-        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-        operators.splice(divide, 1);
-        divide = operators.indexOf("÷");
-      }
+        var divide = operators.indexOf("÷");
+        while (divide != -1) {
+            numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+            operators.splice(divide, 1);
+            divide = operators.indexOf("÷");
+        }
 
-      var multiply = operators.indexOf("×");
-      while (multiply != -1) {
-        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-        operators.splice(multiply, 1);
-        multiply = operators.indexOf("×");
-      }
+        var multiply = operators.indexOf("×");
+        while (multiply != -1) {
+            numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+            operators.splice(multiply, 1);
+            multiply = operators.indexOf("×");
+        }
 
-      var subtract = operators.indexOf("-");
-      while (subtract != -1) {
-        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-        operators.splice(subtract, 1);
-        subtract = operators.indexOf("-");
-      }
+        var subtract = operators.indexOf("-");
+        while (subtract != -1) {
+            numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+            operators.splice(subtract, 1);
+            subtract = operators.indexOf("-");
+        }
 
-      var add = operators.indexOf("+");
-      while (add != -1) {
-        // using parseFloat is necessary, otherwise it will result in string concatenation :)
-        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
-        operators.splice(add, 1);
-        add = operators.indexOf("+");
-      }
+        var add = operators.indexOf("+");
+        while (add != -1) {
+            // using parseFloat is necessary, otherwise it will result in string concatenation :)
+            numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+            operators.splice(add, 1);
+            add = operators.indexOf("+");
+        }
 
-      input.innerHTML = numbers[0]; // displaying the output
+        input.innerHTML = numbers[0]; // displaying the output
 
-      resultDisplayed = true; // turning flag if result is displayed
+        resultDisplayed = true; // turning flag if result is displayed
     });
 
     // clearing the input on press of clear
     clear.addEventListener("click", function() {
-      input.innerHTML = "";
+        input.innerHTML = "";
     });
 }
 
