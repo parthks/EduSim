5d7853114137d8ca74ee5567a6de6999f0e0fac4
Parth Shah <parthks@umich.edu>
2017-08-02 02:27:58 -0400

    Google Analytics woot woot

--- all-units/secrets/topic-secrets.js
+++ all-units/secrets/topic-secrets.js
@@ -1,16 +1,14 @@
 noteFireTitle = "secrets";
 
 summaryText = function () {
-    var html = '<p>Using the power of submatrices, we can swap manipulate parts of a large Matrix to form a new Matrix!</p>';
-    html += '<p>Since images are simply Matrices, we can convert the image pixel data into ASCII values - text, numbers, symbols etc</p>';
-    html += '<p>The conversion happens by swapping out various submatrices to form new values.</p>'
+    var html = '<p>TO DO.</p>'
     
     return html;
 }
 
 reviewText = function () {
-    var html = '<p>Images are nothing but Matrices! </p>';
-    html += '<p>The pixels are stored as red, green, blue and alpha numeric values that can easily be manipulated.';
+    var html = '<p>TO DO </p>';
+    html += '<p>SOON</p>';
     return html;
 }
 
@@ -21,19 +19,14 @@ addVideoDetails = function(){
 }
 
 extraInfo = function() {
-    var html = '<p>While making ASCII Art, some images can be represented beautifully, while others not so much!</p>';
-    html += '<p>Why is this? Can you figure out a way around that?</p>';
-    html += '<br><p>Some images require a bit of "tweaking" to get them to look right! Try experimenting and see the difference it makes!</p>';
-    html += "Don't give up! Enthusiasts have recreated the entire 'Star Wars: A New Hope' movie using ascii values! LOOK IT UP!";
+    var html = '<p>TO DO</p>';
     
     return html;
 }
 
 connections = function() {
-    var html = '<p>ASCII Art relies on the fundamental concept of Linear Algebra, that is, Matrices!</p>'
-    html += '<p>Images are only one form of data that are represented using Matrices</p>'
-    html += '<p>As you will soon discover, the power of Matrices lies its ability to manipulate vast amounts of data quickly and *cough* easily</p>'
-
+    var html = '<p>TO DO</p>'
+    
     return html;
 }
 
