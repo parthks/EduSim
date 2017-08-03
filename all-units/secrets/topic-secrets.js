d226a6bf18f2ee5ecb6d42272435e346453a809b
Parth Shah <parthks@umich.edu>
2017-08-03 01:52:45 -0400

    Content to Secret

--- all-units/secrets/topic-secrets.js
+++ all-units/secrets/topic-secrets.js
@@ -9,8 +9,8 @@ summaryText = function () {
 }
 
 reviewText = function () {
-    var html = '<p>TO DO </p>';
-    html += '<p>SOON</p>';
+    var html = '<p>Linear Algebra is about doing algebra with linear terms where the variables are often vectors and matrices</p>';
+    html += '<p>Matrices can be thought of as a collection of column vectors and are very useful for storing and manipulating data!</p>';
     return html;
 }
 
@@ -21,13 +21,15 @@ addVideoDetails = function(){
 }
 
 extraInfo = function() {
-    var html = '<p>TO DO</p>';
+    var html = '<p>Linear Transformations in the form of Ax = b is the foundation upon which transformations take place!</p>';
+    html += '<p>The process of Gausian Elimination can also be represented as a transformation, and the matrix to do that is called the Elimination Matrix</p>';
     
     return html;
 }
 
 connections = function() {
-    var html = '<p>TO DO</p>'
+    var html = '<p>You will see Ax = b in many areas as you continue to learn linear algebra and beyond!</p>';
+    html += '<p>The next seciton, for example, is all about transformations and how different matrices have unique properties and can manipulate data in fascinating ways!</p>';
     
     return html;
 }
